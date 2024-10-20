790d92f5e0346150181946955fe38326cc1aeb90
Rai abreu <rai.abreu.machado@gmail.com>
2024-10-20 17:31:47 -0300

    Inicio do projeto

---
+++ screenmatch/src/test/java/br/com/alura/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests implements CommandLineRunner {
+
+	@Test
+	void contextLoads() throws Exception {
+		run();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Stranger+Things&apikey=e0dc7e17");
+		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
